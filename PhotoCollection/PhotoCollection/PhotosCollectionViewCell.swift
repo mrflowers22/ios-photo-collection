49cfd62964d07a6402e89870a4dd43eadacd561d
Michael R.F <mrflowers22@uwalumni.com>
2019-05-02 17:55:26 -0400

    on the storyboard, I added a collection view and embedded it into a navigation controller and added a label and image view to the collection view cell.

---
+++ PhotoCollection/PhotoCollection/PhotosCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  PhotosCollectionViewCell.swift
+//  PhotoCollection
+//
+//  Created by Michael Flowers on 5/2/19.
+//  Copyright © 2019 Lambda, Inc. All rights reserved.
+//
+
+import UIKit
+
+class PhotosCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var label: UILabel!
+}
