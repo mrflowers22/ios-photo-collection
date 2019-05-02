daae748b8b21878e7d2a3bb0b289ff5e02099991
Michael R.F <mrflowers22@uwalumni.com>
2019-05-02 16:15:15 -0400

    added model struct and conformed to equatable protocol

---
+++ PhotoCollection/PhotoCollection/Photo.swift
@@ -0,0 +1,14 @@
+//
+//  Photo.swift
+//  PhotoCollection
+//
+//  Created by Michael Flowers on 5/2/19.
+//  Copyright © 2019 Lambda, Inc. All rights reserved.
+//
+
+import Foundation
+
+struct Photo: Equatable {
+    var imageData: Data
+    var title: String
+}
