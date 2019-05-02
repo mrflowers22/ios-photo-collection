bcf10d37b916f4ff957816174ee0e571d0d36327
Paul Solt <PaulSolt@gmail.com>
2019-05-02 14:21:50 -0400

    Added starter Xcode Project

---
+++ PhotoCollection/PhotoCollection/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PhotoCollection
+//
+//  Created by Paul Solt on 5/2/19.
+//  Copyright © 2019 Lambda, Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
