ad2b08ac2f8c25f0526e9d5977cc5f5295d36f8b
Michael R.F <mrflowers22@uwalumni.com>
2019-05-03 08:46:29 -0400

    implemented the logic for the collection view cell.

--- PhotoCollection/PhotoCollection/Views/Collection View Cells/PhotosCollectionViewCell.swift
+++ PhotoCollection/PhotoCollection/Views/Collection View Cells/PhotosCollectionViewCell.swift
@@ -10,7 +10,28 @@ import UIKit
 
 class PhotosCollectionViewCell: UICollectionViewCell {
     
+    var photo: Photo? {
+        didSet {
+            //observe this property so that when it is not nil, which means its holding a value, we want to update the views
+            updateViews()
+        }
+    }
+    
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var label: UILabel!
-    //ths is what im going to do
+    
+    
+    func updateViews(){
+        guard let passedInPhoto = photo else { return }
+        
+        //turn the image data into a UIImage
+        let imageData = passedInPhoto.imageData
+        guard let image = UIImage(data: imageData) else { print("do not have an image to display") ; return }
+        
+        //now that we have a UIImage we can update the image property on imageView to update the view
+        imageView.image = image
+        
+        //update the text property on the label
+        label.text = passedInPhoto.title
+    }
 }
